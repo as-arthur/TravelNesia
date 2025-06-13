e18818fcfe25fea8b6779d7ab519f2622c1d513d
olavliv <165972933+olavliv@users.noreply.github.com>
2025-06-13 19:33:32 +0700

    Create config.js

---
+++ Backend/config/config.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || 86400
+};
