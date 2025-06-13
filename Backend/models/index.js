f8e157c062248d5b1db58f04a252a4d7f1ec583d
olavliv <165972933+olavliv@users.noreply.github.com>
2025-06-13 19:42:19 +0700

    Create index.js

---
+++ Backend/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./user.model');
+const Place = require('./place.model');
+const Visit = require('./visit.model');
+const Rating = require('./rating.model');
+
+module.exports = {
+  User,
+  Place,
+  Visit,
+  Rating
+};
